ea0d301dc66d5d529a924747ec00f733093bde0f
roy <roy@digitalxi.com>
2018-10-08 17:20:19 -0400

    change folder name

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
-// Material
+// Material Modules
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material.module';
+
+// Components
 import { AccomodationComponent } from './accomodation/accomodation.component';
 import { RoomsComponent } from './accomodation/rooms/rooms.component';
-import { FoodBeverageComponent } from './food-beverage/food-beverage.component';
-import { RestaurantComponent } from './food-beverage/restaurant/restaurant.component';
-import { BarsComponent } from './food-beverage/bars/bars.component';
+import { FoodAndBeverageComponent } from './food-and-beverage/food-and-beverage.component';
+import { RestaurantComponent } from './food-and-beverage/restaurant/restaurant.component';
+import { BarsComponent } from './food-and-beverage/bars/bars.component';
 import { LifestyleComponent } from './lifestyle/lifestyle.component';
 import { WelnessAndSpaComponent } from './lifestyle/welness-and-spa/welness-and-spa.component';
 import { ResortActivitiesComponent } from './lifestyle/resort-activities/resort-activities.component';
@@ -30,7 +32,7 @@ import { SignupComponent } from './auth/signup/signup.component';
     AppComponent,
     AccomodationComponent,
     RoomsComponent,
-    FoodBeverageComponent,
+    FoodAndBeverageComponent,
     RestaurantComponent,
     BarsComponent,
     LifestyleComponent,
